64bcc421581f9c05f88da265a889e987118ec4ad
matao <matao@hua-cloud.com.cn>
2020-07-28 14:56:13 +0800

    登录退出

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import "./plugins/element.js"
+import 'element-ui/lib/theme-chalk/index.css';
+import "./assets/fonts/iconfont.css"
+// 导入全局样式表
+import "@/assets/css/global.css"
+
+import axios from "axios"
+// 配置请求的跟路径
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/login'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
