55c210f7098a8675985ba1a95749d3e882316ded
matao <matao@hua-cloud.com.cn>
2020-07-29 14:37:53 +0800

    用户数据

--- src/main.js
+++ src/main.js
@@ -9,7 +9,13 @@ import "@/assets/css/global.css"
 
 import axios from "axios"
 // 配置请求的跟路径
-axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/login'
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1'
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,31 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from "../components/Login.vue"
 import Home from "../components/Home.vue"
+import Users from '../components/user/Users.vue'
+import Welcome from '../components/Welcome.vue'
+
+
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
 Vue.use(VueRouter)
 
-const routes = [{ path: "/", redirect: "/login" }
-  , { path: "/login", component: Login },
-{ path: "/home", component: Home }
+const routes = [
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users },
+      // { path: '/rights', component: Rights },
+      // { path: '/roles', component: Roles }
+    ]
+  }
 ]
 
 const router = new VueRouter({
