94619ea4555a001985a251f2dd82df0076410063
matao <matao@hua-cloud.com.cn>
2020-07-31 23:35:09 +0800

    商品列表

--- src/main.js
+++ src/main.js
@@ -5,6 +5,14 @@ import "./plugins/element.js"
 import 'element-ui/lib/theme-chalk/index.css';
 import "./assets/fonts/iconfont.css"
 import TreeTable from "vue-table-with-tree-grid"
+
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// require styles 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 // 导入全局样式表
 import "@/assets/css/global.css"
 
@@ -21,6 +29,20 @@ Vue.prototype.$http = axios
 Vue.component('tree-table', TreeTable)
 Vue.config.productionTip = false
 
+Vue.use(VueQuillEditor)
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 new Vue({
   router,
   render: h => h(App)
